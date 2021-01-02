167b1150c4699c9f2a5dfb31cf97cae37d2b8672
chenrongchao <chenmiaochaonp@gmail.com>
2021-01-03 00:48:51 +0900

    jalan API res xml to JSON fin

--- routes/event.js
+++ routes/event.js
@@ -6,8 +6,11 @@ const {
   updateEvent,
   delEvent
 } = require('../controller/event')
+const jalanUrl = require('../serc.json').url
+const xml2js = require('xml2js')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 const loginCheck = require('../middleware/loginCheck')
+const { default: axios } = require('axios')
 
 router.prefix('/api/event')
 
@@ -15,8 +18,28 @@ router.get('/', async function (ctx, next) {
 
     const listData = await getList()
     console.log(listData)
-    // console.log(listData)
     ctx.body = new SuccessModel(listData)
+})
+
+router.get('/jalan', async function (ctx, next) {
+    let params = ctx.query
+    console.log(JSON.stringify(params))
+    console.log(jalanUrl)
+    const xmlData = axios.get(jalanUrl, {params}).then(data => {
+        // console.log(data)
+        return data
+    })
+    const xml = await xmlData
+    // console.log("_____________________",xml.data)
+    xmlParser = new xml2js.Parser()
+    xmlParser.parseString(xml.data, function (err, result) {
+        // console.dir(result.Results.Hotel);
+        var data = result.Results.Hotel
+        console.log(data)
+        // console.log('Done');
+        ctx.body = new SuccessModel(data)
+    })
+
 })
 
 router.get('/:eid', async function (ctx, next) {
